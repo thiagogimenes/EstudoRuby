c5e1f2a0469fcee733309958f0e790f66c7562e6
Thiago Gimenes <59589489+thiagogimenes@users.noreply.github.com>
2020-09-08 23:47:08 -0300

    Validadores e Matches
    Hoje estudei validação com rspec (eql, start_with, end_with, to match, not_to match.

--- ruby/unitarios/spec/avengers_spec.rb
+++ ruby/unitarios/spec/avengers_spec.rb
@@ -23,4 +23,49 @@ describe AvengersHeadQuarter do
         expect(hq.list).to eql ['Spiderman']
     end
 
+    it 'deve adcionar uma lista de vingadores' do
+        hq = AvengersHeadQuarter.new
+
+        hq.put('Thor')
+        hq.put('Hulk')
+        hq.put('Spiderman')
+
+        res = hq.list.size > 0 
+
+        #expect(hq.list).to include 'Thor'
+        expect(hq.list.size).to be > 0
+        #verificando se thor existe na lista
+
+        expect(res).to be true
+    end
+
+    it 'Thor deve ser o primeiro da lista' do
+        hq = AvengersHeadQuarter.new
+
+        hq.put('Thor')
+        hq.put('Hulk')
+        hq.put('Spiderman')
+
+        expect(hq.list).to start_with('Thor')
+    end
+
+    it 'Ironman deve ser o último da lista' do
+        hq = AvengersHeadQuarter.new
+
+        hq.put('Thor')
+        hq.put('Hulk')
+        hq.put('Spiderman')
+        hq.put('Ironman')
+
+        expect(hq.list).to end_with('Ironman')
+    end
+
+    it 'deve conter o sobrenome' do
+        avenger = 'Peter Parker'
+
+        expect(avenger).to match(/Park/)
+        expect(avenger).not_to match(/Thiago/)
+    end
+
+    
 end
