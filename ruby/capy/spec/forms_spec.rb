71f7b5d5748fd7624668179457a597b4e4807b44
Thiago Gimenes <59589489+thiagogimenes@users.noreply.github.com>
2020-09-24 23:32:54 -0300

    Testando formularios
    Teste de formularios utilizando capybara

---
+++ ruby/capy/spec/forms_spec.rb
@@ -0,0 +1,14 @@
+describe 'Forms' do
+
+    it 'login com sucesso' do
+        visit 'https://training-wheels-protocol.herokuapp.com/login'
+
+        fill_in 'userId', with: 'stark'
+        fill_in 'passId', with: 'jarvis!'
+
+        click_button 'Login'
+
+        expect(find('#flash').visible?).to be true
+    end
+    
+end
